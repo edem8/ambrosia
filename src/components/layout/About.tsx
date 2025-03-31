85abe71e9f20d5bcb94c69f7ba7cd3debc90991c
edem8 <eddieblack159@gmail.com>
2025-03-31 23:35:27 +0000

    feat: mobile menu navigation

--- src/components/layout/About.tsx
+++ src/components/layout/About.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function () {
-  return <div className=" w-full h-screen p-6  justify-center flex"></div>;
+  return <div className=" w-full h-screen p-6  justify-center flex">About</div>;
 }
