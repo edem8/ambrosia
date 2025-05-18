564c1c0bf6c68235ea434ed6bbb450dc3456429a
edem8 <eddieblack159@gmail.com>
2025-05-18 21:05:11 +0000

    feat: shop path experiences

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Blog() {
+  return <div className=" flex min-h-screen items-center justify-center">Blog</div>;
+}
---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Testimonial() {
+  return <div className=" flex min-h-screen items-center justify-center">Testimonial</div>;
+}
