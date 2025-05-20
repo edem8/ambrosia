60dbbaa9b4f9f0de397f380d448492f20e9dbbf3
edem8 <eddieblack159@gmail.com>
2025-05-20 09:17:57 +0000

    chore: padding fix

--- src/components/layout/Experience.tsx
+++ src/components/layout/Experience.tsx
@@ -67,7 +67,7 @@ export function Experience() {
 
   return (
     <div className="w-full px-10 md:px-35 lg:px-12 xl:px-24 lg:py-8  ">
-      <p className="place-self-center flex items-center space-x-15 md:space-x-20 pl-4 md:pl-6 tracking-widest text-gray-300 mb-10 md:mb-20 text-xs md:text-base">
+      <p className="place-self-center flex items-center space-x-15 md:space-x-20 pl-6 tracking-widest text-gray-300 mb-10 md:mb-20 text-xs md:text-base">
         <p className="cursor-pointer pb-1 border-b font-semibold ">Work</p>
         <p className="cursor-pointer pb-1 border-b font-semibold">Projects</p>
       </p>
