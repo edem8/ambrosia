e45963f7d19d4106e60302def17c80ca54f6c1a2
edem8 <eddieblack159@gmail.com>
2025-05-22 14:08:28 +0000

    chore:font-resizing

--- src/components/layout/About.tsx
+++ src/components/layout/About.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="w-full gap-4  py-8  text-center flex flex-col items-center">
       <h1 className="font-[Bellefair] title tracking-wide">ABOUT</h1>
-      <div className="py-1  w-full px-4 md:px-6 sm:w-[80%] md:w-[70%] lg:w-[60%]  xl:w-[46.5%]  ">
+      <div className="py-1  w-full px-6 sm:w-[80%] md:w-[70%] lg:w-[60%]  xl:w-[46.5%]  ">
         <p className="  tiny tracking-wide ">
         {/* <span className="tracking-widest cursor-pointer pb-1  text-gray-600 text-lg font-[High-Summit] ">agathamabrose@gmail.com</span><br /> */}
           A product manager who thrives at the intersection of design,
